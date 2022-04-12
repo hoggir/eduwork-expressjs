f501c196955578855112244419e9aa8d6618f4a9
Fadel Giras <fadelpopeye@gmail.com>
2022-04-12 14:41:16 +0700

    mongodb + mongoose

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const cors = require('cors');
-// const productRouter = require("./app/product/routes");
-// const productRouterV2 = require("./app/product_v2/routes");
+const productRouter = require("./app/product/routes");
+const productRouterV2 = require("./app/product_v2/routes");
 const productRouterV3 = require("./app/product_v3/routes");
 const productRouterV4 = require("./app/product_v4/routes");
 const foodRouter = require("./app/food/routes");
@@ -20,14 +20,14 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, "uploads")));
-// app.use("/api/v1", productRouter);
-// app.use("/api/v2", productRouterV2);
+app.use("/api/v1", productRouter);
+app.use("/api/v2", productRouterV2);
 app.use("/api/v3", productRouterV3);
 app.use("/api/v4", productRouterV4);
 app.use("/api/v5", foodRouter);
 app.use((req, res, next) => {
   res.status(404);
-  // res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/plain');
   res.send({
     status: "Failed",
     message: "Resource " + req.originalUrl + " not found",
